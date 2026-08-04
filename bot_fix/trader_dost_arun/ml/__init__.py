832706d484e6fd49d951eb6f493b3e527843111b
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2026-08-05 02:40:24 +0530

    Delete bot_fix directory

--- bot_fix/trader_dost_arun/ml/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Machine-learning utilities for walk-forward validation and feature caching."""
--- bot_fix/trader_dost_arun/newsguard/__init__.py
+++
@@ -1,3 +0,0 @@
-from trader_dost_arun.newsguard.models import ImpactAssessment, NewsEvent, ObservedImpact
-
-__all__ = ["ImpactAssessment", "NewsEvent", "ObservedImpact"]
